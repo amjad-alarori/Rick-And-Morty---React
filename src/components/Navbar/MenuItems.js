48547774ff2ee286cef3324b8e254b4483645029
Amjad Alarori <s1134513@student.windesheim.nl>
2020-12-22 16:55:01 +0000

    Merged PR 2101: Navbar
    navbar is klaar

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,19 @@
+export const MenuItems = [
+
+    {
+        title: 'Docs',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'About',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Help us',
+        url: '#',
+        cName: 'nav-links-mobile'
+    }
+
+]
