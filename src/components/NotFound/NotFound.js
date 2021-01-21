ac00a8bad4250a9733dcc8f53851819c2028d577
Trisjan Mustamu <trisjanm@hotmail.com>
2021-01-21 13:18:07 +0100

    NotFound component

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -2,34 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './NotFound.css';
 
-// class NotFound extends ReactComponent{
-
-//     function NoMatch() {
-//         let location = useLocation();
-//     }
-
-//     render() {
-//         return(    
-//             <div>
-//                 <h1>404 - Not Found</h1>
-//                 <h3>No match for <code>{location.pathname}</code></h3>
-//                 <a href="/">
-//                 Go back to homepage
-//                 </a>
-//             </div>
-//         )
-//     }
-// }
-
 function NotFound() {
     return (
         <div id="main">
-            <div class="fof">
-                <h1>404 - Not Found!</h1>
+    	    <div class="fof">
+        		<h1>Error 404 - Page not found!</h1>
+                <br/>
                 <Link to="/">
-                    Go back to homepage
+                <h5>Go back to homepage</h5>
                 </Link>
-            </div>
+    	    </div>
         </div>
     );
 }
